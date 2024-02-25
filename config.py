83633a9c7f6b8b60679038bb20af5427d9e251dc
Scott <122386478+scottoverend@users.noreply.github.com>
2024-02-25 21:08:19 +0000

    Add files via upload

---
+++ config.py
@@ -0,0 +1,25 @@
+import tweepy
+import logging
+import os
+
+logger = logging.getLogger()
+
+def create_api():
+    consumer_key = os.getenv("SOLMoG7lKt75JThincqUUbCpO")
+    consumer_secret = os.getenv("YE36ydwzSDHNlWobORbdDRU9om0QDyvyRxFm6C98CP7gEvjujt") 
+    access_token = os.getenv("818843796846821376-QjSPSAGs2gj6jiHCCJ9Mx9kSAbkyT6t")
+    access_token_secret = os.getenv("FB3lA1VHzDFYAJqqHYwDGtfp5U3tOJEbx7Kn8xstguot6")
+    
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth, wait_on_rate_limit=True,
+        wait_on_rate_limit_notify=True)
+    try:
+        api.verify_credentials()
+    except Exception as e:
+        logger.error("Error creating API", exc_info=True)
+        raise e
+    logger.info("API created")
+    return api
+    
+# Create API object
---
+++ favretweet.py
@@ -0,0 +1,45 @@
+import tweepy
+import logging
+from config import create_api
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
+
+class FavRetweetListener(tweepy.StreamListener):
+    def __init__(self, api):
+        super().__init__(api)
+        self.api = api
+        self.me = api.me()
+
+    def on_status(self, tweet):
+        logger.info(f"Processing tweet id {tweet.id}")
+        if tweet.in_reply_to_status_id is not None or tweet.user.id == self.me.id:
+            # This tweet is a reply or I'm its author so, ignore it
+            return
+        if not tweet.favorited:
+            # Mark it as Liked, since we have not done it yet
+            try:
+                tweet.favorite()
+            except Exception as e:
+                logger.error("Error on fav", exc_info=True)
+        if not tweet.retweeted:
+            # Retweet, since we have not retweeted it yet
+            try:
+                tweet.retweet()
+            except Exception as e:
+                logger.error("Error on fav and retweet", exc_info=True)
+
+    def on_error(self, status_code):
+        if status_code == 420:
+            # Returning False on_data method in case rate limit occurs.
+            return False
+        logger.error(status_code)
+
+def main(keywords):
+    api = create_api()
+    tweets_listener = FavRetweetListener(api)
+    stream = tweepy.Stream(api.auth, tweets_listener)
+    stream.filter(track=keywords, languages=["en"])
+
+if __name__ == "__main__":
+    main(["nft", "non-fungible token"])
